237cb2d5af55f2e186e323ca7a1b853cfb17c04d
savindunawanjana <savindunawanjana08@gmail.com>
2025-07-29 20:34:40 +0530

    2commit

---
+++ src/main/java/edu/lk/ijse/projectgym/demo76promax/bo/BOFactory.java
@@ -0,0 +1,61 @@
+package edu.lk.ijse.projectgym.demo76promax.bo;
+
+import edu.lk.ijse.projectgym.demo76promax.bo.Custom.impl.*;
+
+import static edu.lk.ijse.projectgym.demo76promax.bo.BOTypes.*;
+
+public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory() {
+
+
+    }
+
+    public static BOFactory getInstance() {
+
+        return boFactory == null ? boFactory = new BOFactory() : boFactory;
+
+    }
+
+    public <T extends SuperBO> T getBOTypes(BOTypes botypes) {
+
+        return switch (botypes) {
+            case ADDCOACH -> (T) new AddcoachBOImpl();
+            case CUSTORMERDEATILESMENU -> (T) new CustomerDetailsMenuBOImpl();
+//            case ADDCLEANER -> (T) new AddcleanerBOImpl();
+//            case ADDITEMS -> (T) new AdditemsBOImpl();
+//            case ADDSUPLAYERS -> (T) new AddsuplayersBOImpl();
+            case CUSTORMERMANEGE -> (T)new CustormerManegeBOImpl();
+           case CUSTORMERDORPAYMENTS -> (T) new CustormerpaymentBOImpl();
+//            case DASHBORD -> (T) new DashboardBOImpl();
+//            case DELETECLENER -> (T) new DeleteCleanerBOImpl();
+//            case DELETECOACH -> (T) new DeleteCoachBOImpl();
+//            case EDITEOTOJENARETMASSEGE -> (T) new EditAutoGenerateMessageBOImpl();
+//            case EMPLOYEEATENDENS -> (T) new EmployeeAttendanceBOImpl();
+//            case EMPLOYEEDATAFROMATABLE -> (T) new EmployeeDataFromTableBOImpl();
+//            case EMPLOYEEPAYMENTS -> (T) new EmployeePaymentsBOImpl();
+//            case EMPLOYEEPAGEDEATILES -> (T) new EmployeePageDetailsBOImpl();
+//            case EMPLOYEEPAGE -> (T) new EmployeePageBOImpl();
+//            case INVENTORYPAGE -> (T) new InventoryPageBOImpl();
+//            case LOGINFORSYSTEM -> (T) new LoginForSystemBOImpl();
+//            case LOGINMANEGER -> (T) new LoginManagerBOImpl();
+//            case LOGIMNOWNER -> (T) new LoginOwnerBOImpl();
+//            case MANEGEEXSAIS -> (T) new ManageExerciseBOImpl();
+//            case MANEGERLOGIN -> (T) new ManagerLoginBOImpl();
+//            case MONTHLYMASSEGESENDPAGE -> (T) new MonthlyMessageSendPageBOImpl();
+//            case MONTHLYRIPORTPAGE -> (T) new MonthlyReportPageBOImpl();
+//            case ODERPAGE -> (T) new OrderPageBOImpl();
+//            case OWNERANDADMINLOGINPAGE -> (T) new OwnerAndAdminLoginPageBOImpl();
+//            case SENDMASSEGEPAGE -> (T) new SendMessagePageBOImpl();
+//            case SENDSHEDULTOCUSTORMER -> (T) new SendScheduleToCustomerBOImpl();
+            case USERDEATILESPAGE -> (T) new UserdeatilesmenuBOImpl();
+            case USERDORPASWORD -> (T) new UserDoorPwuiBOImpl();
+              case USERMANEGE -> (T) new UsermanegeBOImpl();
+
+        };
+
+
+    }
+
+}
